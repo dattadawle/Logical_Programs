e0312953ec83cb959bc75c9f1cb58f0f41a30c70
Dawle Datta Lingram <dattadawle72@gmail.com>
2025-01-16 00:11:31 +0530

    logical

---
+++ 1_diamond/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1_diamond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                int rows = 15;
+                int mid = rows / 2 + 1;
+                int count = 1;
+
+                for (int i = 1; i <= rows; i++)
+
+                {
+                    int spaces = (i <= mid) ? mid - i : i - mid;
+                    for (int sp = 0; sp < spaces; sp++)
+                    {
+                        Console.Write("  ");
+                    }
+                    for (int j = 1; j <= count; j++)
+                    {
+                        Console.Write("* ");
+                    }
+
+                    if (mid > i)
+                    {
+                        count += 2;
+                    }
+                    else
+                    {
+                        count -= 2;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+   
