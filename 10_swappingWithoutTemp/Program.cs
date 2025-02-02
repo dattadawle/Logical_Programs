955e1638ad644c951671b18dbc351c01ba85ea43
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-02-02 10:34:06 +0530

    logical10

---
+++ 10_swappingWithoutTemp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _10_swappingWithoutTemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Numbers without swapping : a= {a} and b={b}");
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"Numbers with swapping : a= {a} and b={b}");
+
+            Console.ReadKey();
+        }
+    }
+}
--- 9_ReverseNumber/Program.cs
+++ 9_ReverseNumber/Program.cs
@@ -5,19 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the number...");
-            int num=int.Parse(Console.ReadLine()); // num=123
+            int num = int.Parse(Console.ReadLine()); // num=123
 
             int rev = 0;
 
             while (num > 0)
             {
                 rev = rev * 10 + num % 10; //rev =3
-                                            // rev=
-                num/= 10;                   // num=12
+                                           // rev=
+                num /= 10;                   // num=12
             }
             Console.WriteLine("Reversed Number=" + rev);
             Console.ReadLine();
         }
-        
+
     }
 }
