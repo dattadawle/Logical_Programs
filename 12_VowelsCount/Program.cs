b2a4c9a43dbeb34ae4c30e0cf86d37e1667a8397
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-03-05 21:50:56 +0530

    stringvowels

---
+++ 12_VowelsCount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string to count the vowels:");
+        string input = Console.ReadLine();
+
+        int vowelCount = CountVowels(input);
+        Console.WriteLine($"The number of vowels in the string is: {vowelCount}");
+    }
+
+    static int CountVowels(string str)
+    {
+        int count = 0;
+        foreach (char c in str.ToLower())
+        {
+            if ("aeiou".Contains(c))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
