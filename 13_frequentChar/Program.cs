392c8f183760295069c1604ef255a8b1b50b216f
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-03-10 23:53:06 +0530

    logical13

---
+++ 13_frequentChar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static char MostFrequentChar(string str)
+    {
+        return str.GroupBy(c => c)
+                  .OrderByDescending(g => g.Count())
+                  .First().Key;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(MostFrequentChar("hello"));  // Output: 'l'
+    }
+}
