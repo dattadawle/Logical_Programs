a71085dcaf90f14752c0d79517776a919215d536
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-03-28 20:07:36 +0530

    TitleCase Program

---
+++ 14_TitleCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static string ToTitleCase(string str)
+    {
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        return textInfo.ToTitleCase(str.ToLower());
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(ToTitleCase("hello world"));  // Output: "Hello World"
+    }
+}
