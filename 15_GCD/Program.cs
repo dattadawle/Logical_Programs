c6973fad2de9ec6039c3383ffdc1a5d8ab73f53a
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-04-06 23:58:56 +0530

    logical GCD

---
+++ 15_GCD/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class GCDExample
+{
+    static int GCD(int a, int b)
+    {
+        return (b == 0) ? a : GCD(b, a % b);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter two numbers: ");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("GCD: " + GCD(a, b));
+    }
+}
