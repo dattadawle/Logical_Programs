bead52e059c28ed8e1f2119c19b76f69d2a87de0
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-05-18 08:33:13 +0530

    logical

--- 16_reverseString/Program.cs
+++ 16_reverseString/Program.cs
@@ -2,12 +2,20 @@
 
 string input = Console.ReadLine();
 
-char[] charArr = input.ToCharArray();
+/*char[] charArr = input.ToCharArray();
 int length = charArr.Length;
 for (int i = length-1; i >=0; i--)
 {
     Console.Write(charArr[i]);
 }
 
-Console.ReadLine();
+Console.ReadLine();*/
+
+
+//another way
+char[] charArray = input.ToCharArray();
+
+Array.Reverse(charArray);
+
+Console.WriteLine(charArray);
 
