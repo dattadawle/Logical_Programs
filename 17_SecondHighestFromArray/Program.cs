4baa0faf7a8fed90e56f4645a8fa07d7cb98f901
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-05-19 17:35:20 +0530

    logical

---
+++ 17_SecondHighestFromArray/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//find the second highest number from array
+int[] arr= new int[]{1,2,2,3,4,4,5,5,5,440};
+
+int secondhigher=0;
+int higher=0;
+
+for (int i = 0; i< arr.Length; i++)
+{
+   
+    if (arr[i] > secondhigher && arr[i] !=higher)
+    {
+        secondhigher = higher;
+        higher = arr[i];
+  
+       
+    }
+
+    else if(arr[i] < secondhigher && arr[i] == secondhigher && higher == arr[i])
+    {
+        continue;
+    }
+
+}
+
+Console.WriteLine($"second highest :{secondhigher}");
+
---
+++ 18_NumberPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+for (int i=1;i<=5;i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+
+        Console.Write(j);
+    }
+   /* for (int j=5-i;j <= 1;j++)
+        { 
+        Console.Write(" ");
+      }*/
+    Console.WriteLine();
+}
